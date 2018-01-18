c0a05da9a6d29d75b9f098da4e455539af74e145
gustavo <gustavo@mail.com>
2018-01-17 21:50:33 -0300

    test

--- assets/js/components/Hello.vue
+++ assets/js/components/Hello.vue
@@ -6,12 +6,12 @@
           <h1 class="title">
            Titulo
           </h1>
-            <div class="nombre">
+ <!--            <div class="nombre">
                 <pre>
                     {{$data}}
                 </pre>
-            </div>
-            <div v-for="item in lista.content"> {{ item.nombre }}</div>
+            </div> -->
+            <div class="nombre" v-for="item in lista.content"> {{ item.nombre }} - {{ item.apellido }}</div>
         <h2 class="subtitle">
           Innovating and making everything simpler
         </h2>
