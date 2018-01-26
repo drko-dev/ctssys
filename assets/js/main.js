4a93a589e60b70ea7647eff49df5d361cd82cc51
gustavo <gustavo@mail.com>
2018-01-25 23:46:12 -0300

    form generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,13 @@ import VueResource from 'vue-resource'
 import router from './router/'
 import Buefy from 'buefy'
 import App from './App.vue'
-
+import VueFormGenerator from "vue-form-generator";
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
 Vue.use(VueRouter);
 Vue.use(VueResource);
+Vue.use(VueFormGenerator);
 
 let appVue = new Vue({
   el: '#vueApp',
