1910c4a3ab83ff8bd90a1294f3f3acaf784e82ab
gustavo <gustavo@mail.com>
2018-01-27 21:37:35 -0300

    form working

--- assets/js/components/Demo.vue
+++ assets/js/components/Demo.vue
@@ -5,8 +5,9 @@
         <div class="content">
           <slot></slot>
         </div>
-        <form class="form is-sample" :action="action" :method="method">
+        <form class="form is-sample" @submit.prevent="onSubmit" :action="action" :method="method">
           <formly-form :form="form" :model="model" :fields="fields"></formly-form>
+           <button type="submit" class="button is-primary">Send</button>
         </form>
       </div>
     </div>
@@ -17,6 +18,21 @@
   import * as controls from 'vue-formly-buefy/dist/controls'
 
   export default {
+    methods: {
+      onSubmit(){
+        // GET /someUrl
+        this.$http.post('/api/contact/', this.model).then(
+            response => {
+              // get body data
+              //this.lista = response.body;
+                console.log(this.model);
+            },
+            response => {
+                // error callback
+                alert('error');
+            });
+      }
+    },
     props: {
       action: {
         type: String,
@@ -31,11 +47,11 @@
       return {
         form: {},
         model: {
-          name: 'John Silver',
-          email: 'john@',
-          username: 'johnsilver',
+          name: '',
+          email: '',
+          username: 'usuario',
           telephone: '',
-          password: 'iwantmytreasure',
+          password: '',
           message: '',
         },
         fields: [
@@ -193,6 +209,9 @@
   /*
    * Override Bulma rules to properly display a non-standard field.
    */
+   fieldset{
+     border: none;
+   }
   .field.is-addons-ordered {
     flex-wrap: wrap;
     position: relative;
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,12 @@ import VueResource from 'vue-resource'
 import router from './router/'
 import Buefy from 'buefy'
 import App from './App.vue'
-import VueFormGenerator from "vue-form-generator";
 import VueFormly from 'vue-formly'
 import VueFormlyBuefy from 'vue-formly-buefy'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 Vue.use(VueResource);
-Vue.use(VueFormGenerator);
 Vue.use(Buefy)
 Vue.use(VueFormly)
 Vue.use(VueFormlyBuefy)
